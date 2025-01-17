f88e6299ede981a3aa8b29a346fa8d0d35663510
Hebert Pena <hebertphilipe2@hotmail.com>
2025-01-17 20:16:02 +0000

    Aventureiro concluido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+//declarar funções
+void get_info(void);
+void print_info(void);
+float calculo_dp(float populacao, float area);
+float calculo_pc(float pib, float populacao);
 
+//declarar variaveis
+char nome[20];
+char codigo[5];
+int populacao, p_turisticos;
+float area, pib, densidade_pop, pib_c;
+
+//Função main chamando todas as outras funções e finalizando com o return 0
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    char nome[20];
-    char codigo[4];
-    int populacao, p_turisticos;
-    float area, pib;
+    get_info();
+    calculo_dp(populacao, area);
+    calculo_pc(pib, populacao);
+    print_info();
+    return 0;
+}
 
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+// Cadastro das Cartas
+// Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+void get_info() {
     printf("Digite o nome da cidade: \n");
     scanf("%s", &nome[0]);
     printf("Digite o código da cidade: \n");
@@ -30,11 +36,10 @@ int main() {
     scanf("%f", &area);
     printf("Digite o PIB da cidade: \n");
     scanf("%f", &pib);
+}
 
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-    //primeiro e ultimo printf apenas pulando linhas por estética
+//Função para imprimir valores, primeiro e ultimo printf com linhas extras por estética
+void print_info() {
     printf("\n\n\n\n\n\n\n");
     printf("Cidade: %s\n", nome);
     printf("Código: %s\n", codigo);
@@ -42,7 +47,19 @@ int main() {
     printf("Pontos turisticos: %d\n", p_turisticos);
     printf("Área: %.2f\n", area);
     printf("PIB: %.2f\n", pib);
+    printf("Densidade populacional: %.2f\n", densidade_pop);
+    printf("PIB per capita: %.2f\n", pib_c);
     printf("\n\n\n\n");
+}
 
-    return 0;
+//função para calculo da densidade populacional
+float calculo_dp(float populacao, float area) {
+    densidade_pop = populacao / area;
+    return densidade_pop;
+}
+
+//função para calculo do PIB per capita
+float calculo_pc(float pib, float populacao) {
+    pib_c = pib / populacao;
+    return pib_c;
 }
